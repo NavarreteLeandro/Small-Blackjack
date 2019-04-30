65cbe6c9bc6d14dea33001499eb2d325edae7ef1
Leandro Navarrete <leandron@lagash.com>
2019-04-29 21:30:55 -0300

    changue script.js

---
+++ script.js
@@ -0,0 +1,87 @@
+//Blackjack by Leandro Navarrete
+
+//Card variables
+let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'],
+    values = ['Ace', 'King', 'Queen', 'Jack',
+    'Ten', 'Nine', 'Eight', 'Seven', 'Six',
+    'Five', 'Four', 'Three', 'Two'];
+
+//Dom variables
+let textArea = document.getElementById('text-area'),
+    newGameButton = document.getElementById('new-game-button'),
+    hitButton = document.getElementById('hit-button'),
+    stayButton = document.getElementById('stay-button');
+
+//Game variables
+let gameStarted = false,
+    gameOver = false,
+    playerWon= false,
+    dealerCards = [],
+    playerCards = [],
+    dealerScore = 0,
+    playerScore= 0,
+    deck = [];
+
+hitButton.style.display = 'none';
+stayButton.style.display = 'none';
+showStatus();
+
+newGameButton.addEventListener('click' , function() {
+    gameStarted = true;
+    gameOver = false;
+    playerWon = false;
+
+    deck = createDeck();
+    shuffleDeck(deck);
+    dealerCards = [getNextCard(), getNextCard()];
+    playerCards = [getNextCard(), getNextCard()];
+
+    showStatus();
+    newGameButton.style.display = 'none';
+    hitButton.style.display = 'inline';
+    stayButton.style.display = 'inline';
+    
+});
+
+function createDeck(){
+  let deck = [];
+  for(let suitIdx = 0; suitIdx < suits.lenght; suitIdx++){
+    for(let valueIdx = 0; valueIdx < values.lenght; valueIdx++){
+        let card = {
+          suit: suits[suitIdx],
+          value: values[valueIdx]
+        };
+        deck.push( card );
+    }
+  }
+  return deck;
+}
+
+function shuffleDeck(deck) {
+  for (let i = 0; i < deck.lenght; i++){
+    let swapIdx = Math.trunc(Math.random() * deck.lenght);
+    let tmp = deck[swapIdx];
+    deck[swapIdx] = deck[i];
+    deck[i] = tmp;
+  }
+}
+
+function getCardString(card){
+  return card.value + ' of ' + card.suit;
+}
+
+function getNextCard(){
+  return deck.shift();
+}
+
+function showStatus() {
+  if (!gameStarted) {
+    textArea.innerText = 'Welcome to Blackjack';
+    return;
+  } 
+  for (let i=0; i<deck.length; i++) {
+    textArea.innerText += '\n' + getCardString(deck[i]);
+    console.log("hello"); 
+  }
+
+}
